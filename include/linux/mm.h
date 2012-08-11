88de64667bc5db05fcee31fc0cc01f02c373b190
scotthartbti <scott.hart.bti@gmail.com>
2012-08-11 11:54:58 -0700

    Revert "vmscan: backport fixes for PVR drivers from crespo 3.0.31"
    This reverts commit 76367b6d67f200c46b74bff88705afabaf686641.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -989,15 +989,6 @@ static inline void sync_mm_rss(struct task_struct *task, struct mm_struct *mm)
 #endif
 
 /*
- * This struct is used to pass information from page reclaim to the shrinkers.
- * We consolidate the values for easier extention later.
- */
-struct shrink_control {
-	gfp_t gfp_mask;
-	unsigned long nr_to_scan;
-};
-
-/*
  * A callback you can register to apply pressure to ageable caches.
  *
  * 'shrink' is passed a count 'nr_to_scan' and a 'gfpmask'.  It should
@@ -1013,7 +1004,7 @@ struct shrink_control {
  * querying the cache size, so a fastpath for that case is appropriate.
  */
 struct shrinker {
-	int (*shrink)(struct shrinker *, struct shrink_control *sc);
+	int (*shrink)(struct shrinker *, int nr_to_scan, gfp_t gfp_mask);
 	int seeks;	/* seeks to recreate an obj */
 
 	/* These are for internal use */
@@ -1438,8 +1429,7 @@ int in_gate_area_no_task(unsigned long addr);
 
 int drop_caches_sysctl_handler(struct ctl_table *, int,
 					void __user *, size_t *, loff_t *);
-unsigned long shrink_slab(struct shrink_control *shrink,
-			unsigned long nr_pages_scanned,
+unsigned long shrink_slab(unsigned long scanned, gfp_t gfp_mask,
 			unsigned long lru_pages);
 
 #ifndef CONFIG_MMU
